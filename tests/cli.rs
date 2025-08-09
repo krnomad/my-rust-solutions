46749bc09d5d38c4e8a4674f3edc74476f71c70a
Jongwon Kang <krnomad@gmail.com>
2025-08-09 23:30:21 +0900

    add new command - assert_cmd

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,9 +1,14 @@
-use std::process::Command;
+// use std::process::Command;
+
+use assert_cmd::Command;
 
 #[test]
 fn work() {
-    let mut cmd = Command::new("hello");
-    let res = cmd.output();
-    assert!(res.is_ok());
-    assert!(true);
+    // let mut cmd = Command::new("hello");
+    // let res = cmd.output();
+    // assert!(res.is_ok());
+    // assert!(true);
+
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success();
 }
