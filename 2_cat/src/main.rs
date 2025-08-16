de226812b8223c159ec3a5feaf9b0562bec18409
jongwon kang <krnomad@gmail.com>
2025-08-16 12:36:38 +0900

    ready for 2 chapter

---
+++ 2_cat/src/main.rs
@@ -0,0 +1,13 @@
+use clap::Parser;
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct Args {
+    #[arg(required(true))]
+    text: Vec<String>,
+
+    #[arg(short('n'), long("no-newline"), help("do not print new line"))]
+    omit_newline: bool,
+}
+fn main() {
+    let _args = Args::parse();
+}
